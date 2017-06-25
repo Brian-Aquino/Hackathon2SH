e8e45b394e16691cbcd637cd3eb842f7e6869ee5
Brian-Aquino <brian.aquino1994@gmail.com>
2017-06-25 05:48:10 -0400

    nn

--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -8,7 +8,6 @@ from FlaskWebProject1 import app
 from flask import request
 import requests
 import json
-import pdb
 @app.route('/')
 @app.route('/home')
 def home():
@@ -21,7 +20,6 @@ def home():
 @app.route('/submit')
 
 def submit():
-    pdb.set_trace()
     a =  request.args.get("text")
     r = requests.post('http://sample-env-3.pixfhe7yjs.us-east-1.elasticbeanstalk.com/langAnalyze', data={"text":request.args.get("text")})
     return r.text
